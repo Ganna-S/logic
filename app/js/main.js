104a71dd04ae103dab2ae5ad08d083fecf1d794e
Ganna-S <gannashutenko@gmail.com>
2020-01-18 00:46:31 +0200

    updated header and slider, added services

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,14 @@ $(function () {
         dots: true,
         arrows: false,
     });
+
+    $('.services__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.services__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.services__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
 });
+
