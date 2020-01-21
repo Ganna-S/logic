19df2c50f3b0a1b1b9ca586033e5bb9415389e72
Ganna-S <gannashutenko@gmail.com>
2020-01-21 23:27:25 +0200

    updated about

--- app/js/main.js
+++ app/js/main.js
@@ -7,10 +7,10 @@ $(function () {
         arrows: false,
     });
 
-    $('.services__tabs .tab').on('click', function (event) {
+    $('.services__tabs .tab, .about__tabs .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
-        $('.services__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.services__tabs .tabs').find('.tab').removeClass('active');
+        $('.services__tabs, .about__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.services__tabs .tabs, .about__tabs .tabs').find('.tab').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
