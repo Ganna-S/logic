241b3f2316707a7924c16cbfc05619baaa8954a0
Ganna-S <gannashutenko@gmail.com>
2020-02-01 18:35:43 +0200

    added project

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,25 @@
 $(function () {
 
-    $('.slider__box, .work__box').slick({
+    $('.slider__box, .work__box, .customers__box').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
         arrows: false,
     });
 
-    $('.services__tabs .tab, .about__tabs .tab').on('click', function (event) {
+    $('.design__tabs .tab, .about__tabs .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
-        $('.services__tabs, .about__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.services__tabs .tabs, .about__tabs .tabs').find('.tab').removeClass('active');
+        $('.design__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.design__tabs .tabs, about__tabs .tabs').find('.tab').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
     });
     
-    $('input[type="file"], select').styler();
+
+
+
+    var mixer = mixitup('.project__inner-box');
+
 });
 
