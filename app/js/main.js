d86642b89c3cd937ce787c9f9e9081fd66083980
Ganna-S <gannashutenko@gmail.com>
2020-02-05 21:29:01 +0200

    updated about tabs

--- app/js/main.js
+++ app/js/main.js
@@ -9,12 +9,13 @@ $(function () {
 
     $('.design__tabs .tab, .about__tabs .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
-        $('.design__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.design__tabs .tabs, about__tabs .tabs').find('.tab').removeClass('active');
+        $('.design__tabs, .about__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.design__tabs .tabs, .about__tabs .tabs').find('.tab').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
     });
+    
 
     $(document).ready(function () {
 
