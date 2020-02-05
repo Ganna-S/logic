f972985d94a2bdb3f71cef66de31d7a7e38c88b9
Ganna-S <gannashutenko@gmail.com>
2020-02-05 23:20:05 +0200

    updated tabs about

--- app/js/main.js
+++ app/js/main.js
@@ -7,15 +7,22 @@ $(function () {
         arrows: false,
     });
 
-    $('.design__tabs .tab, .about__tabs .tab').on('click', function (event) {
+    $('.design__tabs .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
-        $('.design__tabs, .about__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.design__tabs .tabs, .about__tabs .tabs').find('.tab').removeClass('active');
+        $('.design__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.design__tabs .tabs').find('.tab').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
     });
-    
+    $('.about__tabs .tab-a').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.about__tabs').find('.tab-item-a').removeClass('active-tab_content').hide();
+        $('.about__tabs .tabs-about').find('.tab-a').removeClass('active-a');
+        $(this).addClass('active-a');
+        $('#' + id).addClass('active-tab_content').fadeIn();
+        return false;
+    });
 
     $(document).ready(function () {
 
