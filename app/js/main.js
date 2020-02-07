4a2390f3c50a66a1112b066fb100089a6bf10d3e
Ganna-S <gannashutenko@gmail.com>
2020-02-07 21:57:00 +0200

    added animation

--- app/js/main.js
+++ app/js/main.js
@@ -33,6 +33,32 @@ $(function () {
 
     });
 
+    new WOW().init();
+
+    $(document).ready(function () {
+
+        var show = true;
+        var countbox = ".awards__inner";
+        $(window).on("scroll load resize", function () {
+            if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+            var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+            var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+            var w_height = $(window).height(); // Высота окна браузера
+            var d_height = $(document).height(); // Высота всего документа
+            var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+            if (w_top + 100 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+                $('.inner__number').css('opacity', '1');
+                $('.inner__number').spincrement({
+                    thousandSeparator: "",
+                    duration: 1200
+                });
+    
+                show = false;
+            }
+        });
+    
+    });
+  
 
 
     var mixer = mixitup('.project__inner-box');
