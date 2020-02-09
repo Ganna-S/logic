aca65423550d3b290a193f746ac6fb42024de596
Ganna-S <gannashutenko@gmail.com>
2020-02-09 22:05:33 +0200

    added header and slider adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -66,6 +66,10 @@ $(function () {
         console.log(w_top + " " + e_top);
     });
 
+    $('.header__menu-btn').on('click', function () {
+        $('.header__menu ul').slideToggle();
+    });
+
     var mixer = mixitup('.project__inner-box');
 
 });
