449c8e7e313b1ce5712acf5265970291994ac3ed
Ganna-S <gannashutenko@gmail.com>
2020-04-10 13:43:40 +0300

    adaptive updated

--- app/js/main.js
+++ app/js/main.js
@@ -70,6 +70,15 @@ $(function () {
         $('.header__menu ul').slideToggle();
     });
 
+    $(document).ready(function(){
+        $("#menu").on("click","a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 500);
+        });
+    });
+
     var mixer = mixitup('.project__inner-box');
 
 });
